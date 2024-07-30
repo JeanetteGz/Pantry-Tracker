c562cb700eb5f831da7dd436a6dfd828dc55a828
Jeanettegz <Jeanetteglz99@gmail.com>
2024-07-29 23:13:58 -0400

    Added boxes to display items

---
+++ pantry-tracker/app/page.js
@@ -0,0 +1,37 @@
+import { Box, Stack } from "@mui/material";
+
+const item = [
+  "Apple",
+  "Banana",
+  "Orange",
+  "Strawberry",
+];
+
+export default function Home() {
+  return ( 
+    <Box 
+      width="100vw" 
+      height="100vh"
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'center'}
+    >
+      <Stack width="800px" height="600px" spacing={2}>
+        
+        {item.map((i) => (
+          <Box
+            key={i}
+            width="100px"
+            height="100px"
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            bgcolor={"primary.main"}
+          >
+            {i}
+        </Box>
+      ))}
+    </Stack>
+  </Box>
+  );
+}
