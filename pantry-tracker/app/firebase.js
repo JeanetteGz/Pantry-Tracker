496c7931d8df1d91215aa207505fd447124a6f3b
Jeanettegz <Jeanetteglz99@gmail.com>
2024-07-31 01:25:06 -0400

    Made a quick change

--- pantry-tracker/app/firebase.js
+++ pantry-tracker/app/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0TIOBQlwud98-8Lc1K1fVi5gDcD9eq5E",
-  authDomain: "pantry-tracker-ffae6.firebaseapp.com",
-  projectId: "pantry-tracker-ffae6",
-  storageBucket: "pantry-tracker-ffae6.appspot.com",
-  messagingSenderId: "617967360444",
-  appId: "1:617967360444:web:06e1a889d5bd73249033b9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
