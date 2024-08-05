33f99f4df60d54e21c9d1abb29001de0a78cd9af
Jeanettegz <Jeanetteglz99@gmail.com>
2024-08-05 08:27:28 -0400

    Fixed syntx error

--- pantry-tracker/next.config.js
+++ pantry-tracker/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/app', // Set this to the base path if your app is deployed in a subdirectory
 };
 
-export default nextConfig;
+module.exports = nextConfig;
